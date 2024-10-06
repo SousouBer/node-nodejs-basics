daa3ec3dd36288601ba565deae478c7184621ff4
Soso <soso.beriashvili@redberry.ge>
2024-10-06 18:08:15 +0400

    feat: add functionality to create a file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "node:fs/promises";
+
+import { existsSync } from "node:fs";
+
+const fileContent = "I am fresh and young";
+const filePath = "files/fresh.txt";
+const errorMessage = "FS operation failed";
+const successMessage = "File created";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    if (existsSync(filePath)) {
+      throw new Error(errorMessage);
+    }
+
+    await fs.writeFile(filePath, fileContent);
+    console.log(successMessage);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await create();
