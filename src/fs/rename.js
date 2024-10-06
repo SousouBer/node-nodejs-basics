f2cc2341e2f64dcead2990eecf6eb8e28614208a
Soso <soso.beriashvili@redberry.ge>
2024-10-06 18:53:06 +0400

    feat: add functionality to rename a file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+
+import { existsSync } from "node:fs";
+
+const filePath = "files/wrongFilename.txt";
+const correctedFilePath = "files/properFilename.md";
+
+const errorMessage = "FS operation failed";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(filePath) || existsSync(correctedFilePath)) {
+      throw new Error(errorMessage);
+    }
+
+    await fs.rename(filePath, correctedFilePath);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await rename();
