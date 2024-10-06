c9bf92807bcd82eeb8fa88c2170b59bbdabe8c58
Soso <soso.beriashvili@redberry.ge>
2024-10-06 19:21:09 +0400

    feat: add functionality to console log the array of files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+
+import { existsSync } from "node:fs";
+
+const folderPath = "files";
+const errorMessage = "FS operation failed";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(folderPath)) {
+      throw new Error(errorMessage);
+    }
+
+    const arrayOfFiles = await fs.readdir(folderPath);
+    console.log(arrayOfFiles);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await list();
