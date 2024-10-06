8dfc8cf5e8058612976b2814d250244bd307bc0e
Soso <soso.beriashvili@redberry.ge>
2024-10-06 19:24:01 +0400

    feat: add functionality to read content from a file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+
+import { existsSync } from "node:fs";
+
+const folderPath = "files/fileToRead.txt";
+const errorMessage = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(folderPath)) {
+      throw new Error(errorMessage);
+    }
+
+    const fileData = await fs.readFile(folderPath, { encoding: "utf8" });
+    console.log(fileData);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await read();
