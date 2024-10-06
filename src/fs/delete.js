0f2f61de1c09993152e0c02ca2ceb5bdac607a53
Soso <soso.beriashvili@redberry.ge>
2024-10-06 19:10:52 +0400

    feat: add functionality to remove a file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+
+import { existsSync } from "node:fs";
+
+const filePath = "files/fileToRemove.txt";
+const errorMessage = "FS operation failed";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(filePath)) {
+      throw new Error(errorMessage);
+    }
+
+    await fs.unlink(filePath);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await remove();
