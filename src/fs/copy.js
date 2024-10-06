87cd82558d4973513a76de2c8316e307c1cabb7a
Soso <soso.beriashvili@redberry.ge>
2024-10-06 18:41:40 +0400

    feat: add functionality to copy folder with its files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "fs/promises";
+
+import { existsSync } from "node:fs";
+
+const folderPath = "files";
+const copyFolderPath = "files_copy";
+const errorMessage = "FS operation failed";
+const successMessage = "Copied successfully";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(folderPath) || existsSync(copyFolderPath)) {
+      throw new Error(errorMessage);
+    }
+
+    await fs.cp(folderPath, copyFolderPath, { recursive: true });
+    console.log(successMessage);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await copy();
