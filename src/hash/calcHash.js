541bd57bffe8d0604b60849e453b2fa03748f4a3
Soso <soso.beriashvili@redberry.ge>
2024-10-07 15:25:12 +0400

    feat: implement function to calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,35 @@
+import fs from "node:fs";
+import crypto from "node:crypto";
+
+import url from "node:url";
+import path from "node:path";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFileHash = (path) =>
+  new Promise((resolve, reject) => {
+    const hash = crypto.createHash("sha256");
+    const rs = fs.createReadStream(path);
+
+    rs.on("error", reject);
+    rs.on("data", (chunk) => hash.update(chunk));
+    rs.on("end", () => resolve(hash.digest("hex")));
+  });
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.resolve(
+    __dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  try {
+    const hashValue = await getFileHash(filePath);
+    console.log(hashValue);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await calculateHash();
