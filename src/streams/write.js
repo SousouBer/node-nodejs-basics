3c82d59c29d695827aa2753571e48c1a8b4e2f6b
Soso <soso.beriashvili@redberry.ge>
2024-10-07 17:12:10 +0400

    feat: implement writing a stream functionality

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from "node:fs";
+import url from "node:url";
+import path from "node:path";
+
+import { stdin } from "node:process";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, "files", "fileToWrite.txt");
+  const ws = fs.createWriteStream(filePath);
+
+  stdin.pipe(ws);
 };
 
 await write();
